6acf4cc06d7733ce8ce41b2b88e2915474f2f478
DRomanArch <dugin.roman2017@gmail.com>
2023-05-02 10:57:48 +0300

    Lesson 2

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -10,7 +10,23 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c = '*';
+
+            Drow(x1,y1,c);
+
+
+            Console.ReadKey();
+        }
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
